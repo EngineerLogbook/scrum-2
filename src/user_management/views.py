89dd1a72e330028f0208a37ad992ba6d9ce444d4
sanchit36 <sanchitbhadgal36@gmail.com>
2021-03-22 21:30:24 +0530

    moved the email logic to signal.py send_email

--- src/user_management/views.py
+++ src/user_management/views.py
@@ -21,24 +21,10 @@ from django.views.generic import (
     DeleteView
 )
 
-from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_bytes, force_text
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.template.loader import render_to_string
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
 from .tokens import account_activation_token
-from django.core.mail import EmailMessage
 from .models import Phase, Member
-
-import threading
-
-
-class EmailThread(threading.Thread):
-    def __init__(self, email_message):
-        self.email_message = email_message
-        threading.Thread.__init__(self)
-
-    def run(self):
-        self.email_message.send()
 
 
 class RedirectingLoginView(LoginView):
@@ -55,25 +41,14 @@ class UserAPIView(generics.ListCreateAPIView):
 
 
 def register(request):
+    if request.user.is_authenticated:
+        return redirect('log-list')
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.is_active = False
             user.save()
-            current_site = get_current_site(request)
-            mail_subject = 'Activate your logbook account.'
-            message = render_to_string('user_management/email/acc_active_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(
-                mail_subject, message, to=[to_email]
-            )
-            EmailThread(email).start()
             messages.info(
                 request,
                 f'Please confirm your email address to complete the registration')
@@ -93,9 +68,10 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
+        login(request, user)
         messages.success(
             request,
-            f'Thank you for your email confirmation. Now you can login your account.')
+            f'Thank you for your email confirmation.')
         return redirect('login')
     else:
         return HttpResponse('Activation link is invalid!')
